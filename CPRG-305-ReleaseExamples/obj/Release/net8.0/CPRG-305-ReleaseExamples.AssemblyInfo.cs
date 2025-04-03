755f9180500b43f6d57f8deee17b26f2926045ba
Quan Huynh <152340025+quanhuynh11@users.noreply.github.com>
2025-04-03 15:36:47 -0600

    woah...

--- CPRG-305-ReleaseExamples/obj/Release/net8.0/CPRG-305-ReleaseExamples.AssemblyInfo.cs
+++ CPRG-305-ReleaseExamples/obj/Release/net8.0/CPRG-305-ReleaseExamples.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CPRG-305-ReleaseExamples")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cea3533f29a91c8d5b0f4966250540133dc3f7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CPRG-305-ReleaseExamples")]
